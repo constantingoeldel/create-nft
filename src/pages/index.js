39b8b758545195e815e0c9c63f84fab69a650498
Constantin Goeldel <constantin_goeldel@t-online.de>
2021-09-15 12:56:26 +0200

    updates landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,71 +9,67 @@ import StatsBox from '../components/StatsBox';
 import customerData from '../data/customer-data';
 import HeroImage from '../svg/HeroImage';
 import SvgCharts from '../svg/SvgCharts';
+import {Widget, Popover, PopupButton} from '@typeform/embed-react'
 
 const Index = () => (
-  <Layout>
+  <>
     <section className="pt-20 md:pt-40">
-      <div className="container mx-auto px-8 lg:flex">
-        <div className="text-center lg:text-left lg:w-1/2">
+      <div className="container mx-auto px-8 lg:flex ">
+        <div className="text-center ">
           <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-            Main title of your landing page
+            The easiest way to create your own NFTs and tokens on Cardano
           </h1>
           <p className="text-xl lg:text-2xl mt-6 font-light">
-            Free landing page template to promote your business startup and generate leads for the
-            offered services
+            It will take just 5 minutes, no prior knowledge required. Payment via ADA or credit card. 
           </p>
-          <p className="mt-8 md:mt-12">
-            <Button size="lg">Get Started</Button>
-          </p>
-          <p className="mt-4 text-gray-600">Sed fermentum felis ut cursu</p>
-        </div>
-        <div className="lg:w-1/2">
-          <HeroImage />
+
         </div>
       </div>
+        <div className="container mx-auto px-8 lg:flex my-6 ">
+          <Widget id="AqmhHkBs" height="500" className="flex-auto"/>
+        </div>
     </section>
-    <section id="features" className="py-20 lg:pb-40 lg:pt-48">
+    <section id="features" className="py-20">
       <div className="container mx-auto text-center">
         <h2 className="text-3xl lg:text-5xl font-semibold">Main Features</h2>
         <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Service One</p>
+              <p className="font-semibold text-xl">Easy</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
+                Don't have any experience with NFTs or crypto? No problem! The process is simple and you can even pay with a credit card!
               </p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Service Two</p>
+              <p className="font-semibold text-xl">Fast</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
+                You will have your NFT in less than 5 minutes. If anything goes wrong, you can personally contact me and we will sort it out
               </p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Service Three</p>
+              <p className="font-semibold text-xl">Versatile</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
+                You can add all sorts of pictures, videos and audio files to yur NFT. For native tokens, you can create as many as you can dream of!
               </p>
             </Card>
           </div>
         </div>
       </div>
     </section>
+    <div className="container mx-auto text-center">
+    <h2 className="text-3xl lg:text-5xl font-semibold">How it works</h2></div>
+
     <SplitSection
       id="services"
       primarySlot={
         <div className="lg:pr-32 xl:pr-48">
-          <h3 className="text-3xl font-semibold leading-tight">Market Analysis</h3>
+          <h3 className="text-3xl font-semibold leading-tight">Add information</h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
-            Our team of enthusiastic marketers will analyse and evaluate how your company stacks
-            against the closest competitors
+            Select between NFT and native token and add details like the name, the symbol, and a description of your new asset.
           </p>
         </div>
       }
@@ -84,11 +80,10 @@ const Index = () => (
       primarySlot={
         <div className="lg:pl-32 xl:pl-48">
           <h3 className="text-3xl font-semibold leading-tight">
-            Design And Plan Your Business Growth Steps
+            Upload media
           </h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
-            Once the market analysis process is completed our staff will search for opportunities
-            that are in reach
+            We will store whatever you send us, it can be audio, videos or pictures. These are forever connected to your NFT.
           </p>
         </div>
       }
@@ -98,17 +93,16 @@ const Index = () => (
       primarySlot={
         <div className="lg:pr-32 xl:pr-48">
           <h3 className="text-3xl font-semibold leading-tight">
-            Search For Performance Optimization
+            Connect a wallet and pay for transaction fees 
           </h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
-            With all the information in place you will be presented with an action plan that your
-            company needs to follow
+            Enter the wallet that the token will be sent to. Then select between paying with ADA or with credit card to cover the cost of creating and sending your NFT.
           </p>
         </div>
       }
       secondarySlot={<SvgCharts />}
     />
-    <section id="stats" className="py-20 lg:pt-32">
+    {/* <section id="stats" className="py-20 lg:pt-32">
       <div className="container mx-auto text-center">
         <LabelText className="text-gray-600">Our customers get results</LabelText>
         <div className="flex flex-col sm:flex-row mt-8 lg:px-24">
@@ -123,8 +117,8 @@ const Index = () => (
           </div>
         </div>
       </div>
-    </section>
-    <section id="testimonials" className="py-20 lg:py-40">
+    </section> */}
+    {/* <section id="testimonials" className="py-20 lg:py-40">
       <div className="container mx-auto">
         <LabelText className="mb-8 text-gray-600 text-center">What customers are saying</LabelText>
         <div className="flex flex-col md:flex-row md:-mx-3">
@@ -135,17 +129,17 @@ const Index = () => (
           ))}
         </div>
       </div>
-    </section>
+    </section> */}
     <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
-      <h3 className="text-5xl font-semibold">Ready to grow your business?</h3>
+      <h3 className="text-5xl font-semibold">Ready to create your NFT?</h3>
       <p className="mt-8 text-xl font-light">
-        Quis lectus nulla at volutpat diam ut. Enim lobortis scelerisque fermentum dui faucibus in.
+        Everything until the final confirmation is easily reversible, no hidden costs.
       </p>
       <p className="mt-8">
-        <Button size="xl">Get Started Now</Button>
+        <Button size="xl"><PopupButton id="AqmhHkBs">Get started now</PopupButton></Button>
       </p>
     </section>
-  </Layout>
+    </>
 );
 
 export default Index;
